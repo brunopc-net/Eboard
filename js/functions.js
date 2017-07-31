696a37593e401d2a0ef77a24a29cf3c35d9d0d22
rollingpacman <admin@servicebpc.com>
2017-07-31 00:42:18 -0400

    testing

--- js/functions.js
+++ js/functions.js
@@ -8,22 +8,23 @@ function setCookie(cname, cvalue, exdays) {
 function getCookie(cname) {
 	var name = cname + "=";
 	var decodedCookie = decodeURIComponent(document.cookie);
-		var ca = decodedCookie.split(';');
+	var ca = decodedCookie.split(';');
+	
 	for(var i = 0; i <ca.length; i++) {
 		var c = ca[i];
 		while (c.charAt(0) == ' ') {
 			c = c.substring(1);
-			}
+		}
 		if (c.indexOf(name) == 0) {
 			return c.substring(name.length, c.length);
-			}
+		}
 	}
 	return "";
 }
 
 function redirect(){
 	var lang = getCookie('lang')
-
+	alert(lang);
 	if (lang == 'fr') {
 		window.location.replace("fr.html");
 	}
